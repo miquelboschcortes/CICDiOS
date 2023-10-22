639d6cbcb8ce4ec64d57930a437e288291bc2898
Miquel Bosch <miquel.bosch.cortes@playsportsnetwork.com>
2023-10-22 19:00:32 +0200

    Initial Commit

---
+++ jubilacio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jubilacio
+//
+//  Created by Miguel Bosch Cortés on 22/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
